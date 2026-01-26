7de37d3721a2c4aca365a4c6b5982aa8f1101a9d
jiahangxu <jiahangxu@microsoft.com>
2026-01-26 09:54:06 +0000

    upload search_r1 testing scripts

--- contrib/recipes/search_r1/search_r1_agent.py
+++ contrib/recipes/search_r1/search_r1_agent.py
@@ -37,10 +37,10 @@ def eval(prediction: str, ground_truth: List[str]) -> float:
 
 def postprocess_response(response: str) -> str:
     """Process responses to stop at search operation or answer operation."""
-    if "</search>" in response:
-        response = response.split("</search>")[0] + "</search>"
-    elif "</answer>" in response:
-        response = response.split("</answer>")[0] + "</answer>"
+    # if "</search>" in response:
+    #     response = response.split("</search>")[0] + "</search>"
+    # elif "</answer>" in response:
+    #     response = response.split("</answer>")[0] + "</answer>"
     return response
 
 
@@ -97,13 +97,13 @@ def passages2string(retrieval_result: List[RetrievalItem]) -> str:
 def call_llm(
     llm_client: OpenAI,
     model_name: str,
-    content: str,
+    messages: List[dict] = [],
     temperature: float = 1.0,
     max_tokens: int = 500,
 ) -> str:
     response = llm_client.chat.completions.create(
         model=model_name,
-        messages=[{"role": "user", "content": content}],
+        messages=messages,
         temperature=temperature,
         max_tokens=max_tokens,
     )
@@ -148,36 +148,37 @@ class SearchR1Agent(LitAgent[Dict[str, Any]]):
 
         turn_id = 0
         finished_flag = False
-        rollout_content: str = ""
+        hist_messages: List[Dict[str, Any]] = [{"role": "user", "content": prompt}]
 
         try:
             while turn_id < self.max_turns and not finished_flag:
                 turn_id += 1
                 turn_response = call_llm(
-                    client, llm.model, prompt + rollout_content, temperature=temperature, max_tokens=500
+                    client, llm.model, messages=hist_messages, temperature=temperature, max_tokens=500
                 )
                 valid_turn_response = postprocess_response(turn_response)
-                rollout_content += valid_turn_response
+                hist_messages.append({"role": "assistant", "content": valid_turn_response})
                 turn_env_feedback = execute_response(valid_turn_response)
                 if len(turn_env_feedback) == 0:
                     finished_flag = True
                 else:
-                    rollout_content += turn_env_feedback
+                    hist_messages.append({"role": "user", "content": turn_env_feedback})
                 logger.info(f"TURN ID {turn_id} | RESP: {turn_response} | ENV FEEDBACK: {turn_env_feedback}")
 
             if not finished_flag:
                 turn_response = call_llm(
-                    client, llm.model, prompt + rollout_content, temperature=temperature, max_tokens=500
+                    client, llm.model, messages=hist_messages, temperature=temperature, max_tokens=500
                 )
-                rollout_content += turn_response
+                hist_messages.append({"role": "assistant", "content": turn_response})
                 logger.info(f"LAST TURN GENERATE | RESP: {turn_response}")
 
+            last_turn_response = [msg["content"] for msg in hist_messages if msg["role"] == "assistant"][-1]
         except Exception as e:
             logger.exception(f"[Rollout {rollout_id}] Error during rollout: {e}")
             return None
 
         end_time_rollout = time.time()
-        reward_score = eval(rollout_content, answer_list)
+        reward_score = eval(last_turn_response, answer_list)
         logger.info("[Rollout %s] Reward: %s", rollout_id, reward_score)
         end_time_eval = time.time()
 
@@ -187,7 +188,7 @@ class SearchR1Agent(LitAgent[Dict[str, Any]]):
         )
         logger.info(
             "question: {} answer: {} ground_truth: {} reward: {}".format(
-                task["question"], rollout_content, answer_list, reward_score
+                task["question"], last_turn_response, answer_list, reward_score
             )
         )
         return reward_score
